66b10b2877be307433f0e5d4aa856005d5a9a05f
Catherine Ling <Catherine Ling@LAPTOP-3PPTN9RC>
2025-03-01 22:08:02 +0800

    Add project files.

---
+++ Assignment/AdminLogin.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Assignment
+{
+    partial class AdminLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.btnAdminLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(236, 84);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(297, 29);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Welcome to Admin page";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(118, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username :";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(118, 260);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(109, 22);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "Password :";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(251, 186);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(39, 27);
+            this.lbl3.TabIndex = 3;
+            this.lbl3.Text = "     ";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.Location = new System.Drawing.Point(251, 260);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(39, 27);
+            this.lbl4.TabIndex = 4;
+            this.lbl4.Text = "     ";
+            // 
+            // btnAdminLogin
+            // 
+            this.btnAdminLogin.Location = new System.Drawing.Point(215, 343);
+            this.btnAdminLogin.Name = "btnAdminLogin";
+            this.btnAdminLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnAdminLogin.TabIndex = 5;
+            this.btnAdminLogin.Text = "Enter";
+            this.btnAdminLogin.UseVisualStyleBackColor = true;
+            // 
+            // AdminLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAdminLogin);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl1);
+            this.Name = "AdminLogin";
+            this.Text = "Admin";
+            this.Load += new System.EventHandler(this.Admin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Button btnAdminLogin;
+    }
+}
---
+++ Assignment/Staff.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Assignment
+{
+    partial class Staff
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdmin = new System.Windows.Forms.Button();
+            this.btnManager = new System.Windows.Forms.Button();
+            this.btnChef = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAdmin
+            // 
+            this.btnAdmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdmin.Location = new System.Drawing.Point(163, 231);
+            this.btnAdmin.Name = "btnAdmin";
+            this.btnAdmin.Size = new System.Drawing.Size(115, 48);
+            this.btnAdmin.TabIndex = 1;
+            this.btnAdmin.Text = "Admin";
+            this.btnAdmin.UseVisualStyleBackColor = true;
+            this.btnAdmin.Click += new System.EventHandler(this.btnAdmin_Click);
+            // 
+            // btnManager
+            // 
+            this.btnManager.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnManager.Location = new System.Drawing.Point(349, 231);
+            this.btnManager.Name = "btnManager";
+            this.btnManager.Size = new System.Drawing.Size(115, 48);
+            this.btnManager.TabIndex = 2;
+            this.btnManager.Text = "Manager";
+            this.btnManager.UseVisualStyleBackColor = true;
+            // 
+            // btnChef
+            // 
+            this.btnChef.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChef.Location = new System.Drawing.Point(543, 231);
+            this.btnChef.Name = "btnChef";
+            this.btnChef.Size = new System.Drawing.Size(115, 48);
+            this.btnChef.TabIndex = 3;
+            this.btnChef.Text = "Chef";
+            this.btnChef.UseVisualStyleBackColor = true;
+            // 
+            // Staff
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnChef);
+            this.Controls.Add(this.btnManager);
+            this.Controls.Add(this.btnAdmin);
+            this.Name = "Staff";
+            this.Text = "Staff";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdmin;
+        private System.Windows.Forms.Button btnManager;
+        private System.Windows.Forms.Button btnChef;
+    }
+}
