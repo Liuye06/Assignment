ce750ffdadc15b31ae11993d3ef7735e5e05aa1f
Catherine Ling <catherineling7@gmail.com>
2025-03-10 23:24:14 +0800

    design ui

--- Assignment/Admin.Designer.cs
+++ Assignment/Admin.Designer.cs
@@ -31,9 +31,9 @@
             this.lbl1 = new System.Windows.Forms.Label();
             this.btn_Home = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btn_salesReport = new System.Windows.Forms.Button();
-            this.btn_feedback = new System.Windows.Forms.Button();
-            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_SalesReport = new System.Windows.Forms.Button();
+            this.btn_Feedback = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,7 +53,7 @@
             this.btn_Home.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_Home.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_Home.ForeColor = System.Drawing.Color.Black;
-            this.btn_Home.Location = new System.Drawing.Point(-1, 122);
+            this.btn_Home.Location = new System.Drawing.Point(-2, 122);
             this.btn_Home.Name = "btn_Home";
             this.btn_Home.Size = new System.Drawing.Size(258, 78);
             this.btn_Home.TabIndex = 2;
@@ -66,60 +66,66 @@
             // 
             this.panel1.BackColor = System.Drawing.Color.AntiqueWhite;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.btn_update);
-            this.panel1.Controls.Add(this.btn_feedback);
-            this.panel1.Controls.Add(this.btn_salesReport);
+            this.panel1.Controls.Add(this.btn_Update);
+            this.panel1.Controls.Add(this.btn_Feedback);
+            this.panel1.Controls.Add(this.btn_SalesReport);
             this.panel1.Controls.Add(this.btn_Home);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(257, 576);
+            this.panel1.Size = new System.Drawing.Size(257, 559);
             this.panel1.TabIndex = 3;
             this.panel1.UseWaitCursor = true;
             // 
-            // btn_salesReport
+            // btn_SalesReport
             // 
-            this.btn_salesReport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_salesReport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_salesReport.Location = new System.Drawing.Point(-1, 196);
-            this.btn_salesReport.Name = "btn_salesReport";
-            this.btn_salesReport.Size = new System.Drawing.Size(257, 78);
-            this.btn_salesReport.TabIndex = 3;
-            this.btn_salesReport.Text = "Sales Report";
-            this.btn_salesReport.UseVisualStyleBackColor = true;
-            this.btn_salesReport.UseWaitCursor = true;
+            this.btn_SalesReport.FlatAppearance.BorderColor = System.Drawing.Color.AntiqueWhite;
+            this.btn_SalesReport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_SalesReport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_SalesReport.ForeColor = System.Drawing.Color.Black;
+            this.btn_SalesReport.Location = new System.Drawing.Point(3, 206);
+            this.btn_SalesReport.Name = "btn_SalesReport";
+            this.btn_SalesReport.Size = new System.Drawing.Size(258, 78);
+            this.btn_SalesReport.TabIndex = 6;
+            this.btn_SalesReport.Text = "Sales Report";
+            this.btn_SalesReport.UseVisualStyleBackColor = true;
+            this.btn_SalesReport.UseWaitCursor = true;
             // 
-            // btn_feedback
+            // btn_Feedback
             // 
-            this.btn_feedback.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_feedback.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_feedback.Location = new System.Drawing.Point(-1, 362);
-            this.btn_feedback.Name = "btn_feedback";
-            this.btn_feedback.Size = new System.Drawing.Size(258, 78);
-            this.btn_feedback.TabIndex = 4;
-            this.btn_feedback.Text = "Feedback";
-            this.btn_feedback.UseVisualStyleBackColor = true;
-            this.btn_feedback.UseWaitCursor = true;
-            this.btn_feedback.Click += new System.EventHandler(this.button2_Click);
+            this.btn_Feedback.FlatAppearance.BorderColor = System.Drawing.Color.AntiqueWhite;
+            this.btn_Feedback.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Feedback.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Feedback.ForeColor = System.Drawing.Color.Black;
+            this.btn_Feedback.Location = new System.Drawing.Point(-2, 299);
+            this.btn_Feedback.Name = "btn_Feedback";
+            this.btn_Feedback.Size = new System.Drawing.Size(258, 78);
+            this.btn_Feedback.TabIndex = 7;
+            this.btn_Feedback.Text = "Feedback";
+            this.btn_Feedback.UseVisualStyleBackColor = true;
+            this.btn_Feedback.UseWaitCursor = true;
             // 
-            // btn_update
+            // btn_Update
             // 
-            this.btn_update.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_update.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_update.Location = new System.Drawing.Point(-1, 460);
-            this.btn_update.Name = "btn_update";
-            this.btn_update.Size = new System.Drawing.Size(258, 78);
-            this.btn_update.TabIndex = 5;
-            this.btn_update.Text = "Update User";
-            this.btn_update.UseVisualStyleBackColor = true;
-            this.btn_update.UseWaitCursor = true;
+            this.btn_Update.FlatAppearance.BorderColor = System.Drawing.Color.AntiqueWhite;
+            this.btn_Update.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Update.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Update.ForeColor = System.Drawing.Color.Black;
+            this.btn_Update.Location = new System.Drawing.Point(-2, 393);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(258, 78);
+            this.btn_Update.TabIndex = 8;
+            this.btn_Update.Text = "Update User";
+            this.btn_Update.UseVisualStyleBackColor = true;
+            this.btn_Update.UseWaitCursor = true;
             // 
             // Admin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1154, 576);
+            this.BackColor = System.Drawing.Color.Linen;
+            this.ClientSize = new System.Drawing.Size(1154, 559);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.lbl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -137,8 +143,8 @@
         private System.Windows.Forms.Label lbl1;
         private System.Windows.Forms.Button btn_Home;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button btn_feedback;
-        private System.Windows.Forms.Button btn_salesReport;
-        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.Button btn_Feedback;
+        private System.Windows.Forms.Button btn_SalesReport;
     }
 }
---
+++ Assignment/salesReport.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Assignment
+{
+    partial class salesReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_salesR = new System.Windows.Forms.Label();
+            this.lbl_month = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lbl_salesR
+            // 
+            this.lbl_salesR.AutoSize = true;
+            this.lbl_salesR.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_salesR.Location = new System.Drawing.Point(32, 21);
+            this.lbl_salesR.Name = "lbl_salesR";
+            this.lbl_salesR.Size = new System.Drawing.Size(136, 25);
+            this.lbl_salesR.TabIndex = 0;
+            this.lbl_salesR.Text = "Sales Report";
+            // 
+            // lbl_month
+            // 
+            this.lbl_month.AutoSize = true;
+            this.lbl_month.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_month.Location = new System.Drawing.Point(375, 102);
+            this.lbl_month.Name = "lbl_month";
+            this.lbl_month.Size = new System.Drawing.Size(55, 20);
+            this.lbl_month.TabIndex = 1;
+            this.lbl_month.Text = "Month";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(436, 102);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // salesReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Linen;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lbl_month);
+            this.Controls.Add(this.lbl_salesR);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "salesReport";
+            this.Text = "salesReport";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_salesR;
+        private System.Windows.Forms.Label lbl_month;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
