830b5bf2d0ab035e03ec5c754e7a858d7952252a
Catherine Ling <catherineling7@gmail.com>
2025-03-12 09:44:48 +0800

    design

--- Assignment/salesReport.Designer.cs
+++ Assignment/salesReport.Designer.cs
@@ -28,10 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbl_salesR = new System.Windows.Forms.Label();
             this.lbl_month = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lbl_menuItem = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lbl_salesR = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lbl_month
+            // 
+            this.lbl_month.AutoSize = true;
+            this.lbl_month.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_month.Location = new System.Drawing.Point(483, 106);
+            this.lbl_month.Name = "lbl_month";
+            this.lbl_month.Size = new System.Drawing.Size(55, 20);
+            this.lbl_month.TabIndex = 1;
+            this.lbl_month.Text = "Month";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(544, 102);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // lbl_menuItem
+            // 
+            this.lbl_menuItem.AutoSize = true;
+            this.lbl_menuItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_menuItem.Location = new System.Drawing.Point(81, 102);
+            this.lbl_menuItem.Name = "lbl_menuItem";
+            this.lbl_menuItem.Size = new System.Drawing.Size(87, 20);
+            this.lbl_menuItem.TabIndex = 3;
+            this.lbl_menuItem.Text = "Menu Item";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(174, 102);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(317, 103);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(75, 23);
+            this.btn_search.TabIndex = 5;
+            this.btn_search.Text = "Search";
+            this.btn_search.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(85, 168);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 6;
             // 
             // lbl_salesR
             // 
@@ -43,24 +101,6 @@
             this.lbl_salesR.TabIndex = 0;
             this.lbl_salesR.Text = "Sales Report";
             // 
-            // lbl_month
-            // 
-            this.lbl_month.AutoSize = true;
-            this.lbl_month.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_month.Location = new System.Drawing.Point(375, 102);
-            this.lbl_month.Name = "lbl_month";
-            this.lbl_month.Size = new System.Drawing.Size(55, 20);
-            this.lbl_month.TabIndex = 1;
-            this.lbl_month.Text = "Month";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(436, 102);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 2;
-            // 
             // salesReport
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -68,6 +108,10 @@
             this.BackColor = System.Drawing.Color.Linen;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.lbl_menuItem);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.lbl_month);
             this.Controls.Add(this.lbl_salesR);
@@ -80,9 +124,12 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lbl_salesR;
         private System.Windows.Forms.Label lbl_month;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lbl_menuItem;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lbl_salesR;
     }
 }
