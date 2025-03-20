14fde3082bc57b6d052b2b3fef2d93c6b647224a
Catherine Ling <catherineling7@gmail.com>
2025-03-20 21:32:18 +0800

    login

---
+++ Assignment/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Assignment
+{
+    internal class User
+    {
+        public object ConfigurationManager { get; private set; }    }
+
+    public string login(string un)
+        {
+            string status = null;
+
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionString["myCS"].ToString());
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("select count(*) from users where username=@a and password = @b", con);
+            cmd.Parameters.AddWithValue("@a", username);
+            cmd.Parameters.AddWithValue("@b", password);
+
+            int count = Convert.ToTnt32(cmd.ExecuteScalar());
+            if (count> 0)
+            {
+                SqlCommand cmd2 = new SqlCommand("select count(*) from users where username=@a and password = @b", con);
+
+        }
+
+}
+    }
