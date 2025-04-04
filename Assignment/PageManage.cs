d0fee8d2e62389d7c6004c6bba07a49281773f01
YY <YY@DESKTOP-149362G>
2025-04-04 20:44:55 +0800

    .

---
+++ Assignment/PageManage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment
+{
+    internal class PageManage
+    {
+        private Form _currentForm;
+
+        public SidebarManager(Form startForm)
+        {
+            _currentForm = startForm;
+        }
+
+        public void NavigateTo(Form newForm)
+        {
+            _currentForm.Hide();
+        }
+
+        _currentForm = newForm;
+        _currentForm.FormClosed += (s, e) => Application.Exit();
+        _currentForm.Show();
+}
+}
